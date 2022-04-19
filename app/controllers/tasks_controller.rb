0ed8d60e6b149654446736302690513019412b3d
sugurufukui <2ar.1225@gmail.com>
2022-04-19 19:33:29 +0900

    current_user追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,49 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
+    @tasks = current_user.tasks.recent
   end
 
   def show
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.new(task_params)
+    if @task.save
+      redirect_to root_url, notice: "タスク「#{@task.name}」を登録しました。"
+      #上記と同義
+      # flash[:notice] = "タスク「#{@task.name}」を登録しました"
+      # redirect_to tasls_url
+    else
+      render  :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
+
+  def update
+    @task.update(task_params)
+    redirect_to root_url, notice: "タスク「#{@task.name}」を更新しました。"
+  end
+
+  def destroy
+    task.destroy
+    redirect_to root_url, notice: "タスク「#{task.name}」を削除しました。", status: :see_other
+  end
+
+    private
+
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
+
+    def set_task
+      @task = current_user.tasks.find(params[:id])
+    end
 end
