1d0d2d810b8837915517a79effe74575a1f7e2b2
sugurufukui <2ar.1225@gmail.com>
2022-04-24 10:52:02 +0900

    言語設定追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  # before_action :set_locale
   helper_method :current_user
   before_action :login_required
 
@@ -11,4 +12,8 @@ class ApplicationController < ActionController::Base
   def login_required
     redirect_to login_url unless current_user
   end
+
+  # def set_locale
+  #   I18n.locale = current_user&.locale || :ja #ログインしていなければ日本語
+  # end
 end
