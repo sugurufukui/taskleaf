7c7d9543760f0d298664cf01e9ef763edbea5fc7
sugurufukui <2ar.1225@gmail.com>
2022-05-07 06:40:30 +0900

    csv機能（入力機能完成）

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
 
   has_one_attached :image
-  
+
   def self.ransackable_attributes(auth_object = nil)
     %w[name created_at]
   end
@@ -17,6 +17,27 @@ class Task < ApplicationRecord
   belongs_to :user
 
   scope :recent, -> { order(created_at: :desc)}
+
+  def self.csv_attributes
+    ["name", "description","created_at","updated_at"]
+  end
+
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|
+      csv << csv_attributes
+      csv.each do |task|
+        csv << csv_attributes.map{|attr| task.send(attr) }
+      end
+    end
+  end
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      task = new
+      task.attributes = row.to_hash.slice(*csv_attributes)
+      task.save!
+    end
+  end
 
   private
     # def set_nameless_name
